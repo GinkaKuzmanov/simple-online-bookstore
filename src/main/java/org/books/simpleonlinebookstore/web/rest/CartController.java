3c7223fa19fd83b0fb0103e9c360264bf4cea99e
GinkaKuzmanov <ginkakuzmanova@gmail.com>
2020-07-12 23:26:39 +0300

    security updated

--- src/main/java/org/books/simpleonlinebookstore/web/rest/CartController.java
+++ src/main/java/org/books/simpleonlinebookstore/web/rest/CartController.java
@@ -7,6 +7,7 @@ import org.books.simpleonlinebookstore.services.baseservices.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ public class CartController {
     }
 
     @PostMapping("/books")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<BillResponse> buyBooks(@AuthenticationPrincipal UserDetails userDetails, @RequestBody List<Book> book) {
         String username = userDetails.getUsername();
         BillResponse billResponse = this.cartService.buyBookForUser(username, book);
@@ -38,6 +40,7 @@ public class CartController {
     }
 
     @PostMapping("/music")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<BillResponse> buyMusic(@AuthenticationPrincipal UserDetails userDetails, @RequestBody List<Music> music) {
         String username = userDetails.getUsername();
         BillResponse billResponse = this.cartService.buyMusicForUser(username, music);
